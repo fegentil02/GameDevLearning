0adf01ccc90986bc72c09c5e23a8dfcf6c7bdab6
fernando gentil <fegentil02@gmail.com>
2021-03-04 18:20:17 -0300

    Finished Prototype 1

--- JuniorProgrammerCourse/Prototype 1/Assets/Scripts/PlayerController.cs
+++ JuniorProgrammerCourse/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //gets player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
        //Vroom Vroom it moves things
-       transform.Translate(Vector3.forward * Time.deltaTime * 20); 
+       transform.Translate(Vector3.forward * Time.deltaTime * speed* forwardInput); 
+       transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
