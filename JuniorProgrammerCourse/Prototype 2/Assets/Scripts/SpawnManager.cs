b6af47f5f3d2219be61773cd2c89062e9f7a2abd
Fernando Asevedo Gentil <fegentil02@gmail.com>
2021-03-22 13:48:30 -0300

    Prototype 2 start

---
+++ JuniorProgrammerCourse/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] AnimalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnAnimals();
+        }
+    }
+
+    void SpawnAnimals()
+    {
+        int animalIndex = Random.Range(0, AnimalPrefabs.Length);
+        Vector3 spawnPos =  new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+
+        Instantiate(AnimalPrefabs[animalIndex], spawnPos, AnimalPrefabs[animalIndex].transform.rotation);
+    }
+}
