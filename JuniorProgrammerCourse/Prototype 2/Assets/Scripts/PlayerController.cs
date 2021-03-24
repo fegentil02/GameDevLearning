d6b3ced76894d33b322947fe4029a051d871e089
fernando gentil <fegentil02@gmail.com>
2021-03-24 16:46:01 -0300

    Unit 2 Complete

--- JuniorProgrammerCourse/Prototype 2/Assets/Scripts/PlayerController.cs
+++ JuniorProgrammerCourse/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10f;
-    public float xRange = 10.0f;
+    public float xRange = 20.0f;
 
     public GameObject ProjectilePrefab;
     // Start is called before the first frame update
--- JuniorProgrammerCourse/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ JuniorProgrammerCourse/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,19 +7,18 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] AnimalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnAnimals", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnAnimals();
-        }
+       
     }
 
     void SpawnAnimals()
